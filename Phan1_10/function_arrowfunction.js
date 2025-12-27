ecd3911f342a422818dbbef35ee344bee1ece2ec
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 07:54:48 +0700

    auto-commit

--- Phan1_10/function_arrowfunction.js
+++ Phan1_10/function_arrowfunction.js
@@ -15,3 +15,12 @@ function tinhDienTich_1(x, y) {
 const tinhDienTich_2 = (x, y) =>  x*y;
 console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_1(5, 3)}`);
 console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_2(5, 3)}`);
+/*
+Bài tập 2: Xử lý chuỗi:
+Yêu cầu: Viết một Arrow Function tên là getHelloName(name).
+
+Kết quả: Nếu truyền vào "Danh", hàm trả về chuỗi "Chào bạn, Danh! Chúc một ngày tốt lành.".
+
+Gợi ý: Sử dụng Template String (dấu backtick `) để nối chuỗi cho đẹp.
+*/
+const getHelloName = (name) => console.log(`Chào bạn, ${name}! Chúc một ngày tốt lành.`);
