ffde8446fdb06fc718156829d56970ac0213785e
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 19:53:09 +0700

    auto-commit

--- Phan2_10/mag_2.js
+++ Phan2_10/mag_2.js
@@ -2,13 +2,28 @@
 // let a2 = [4, 5, 6];
 // let a3 = [...a1, ...a2];
 // console.log(a3);
-
 // // Copy object:
 // let obj_1 = {
 //     "name": "David",
 //     "old": 30,
 //     "salary": 3000000
 // };
-
 // let obj_2 = {"sex": "Male", ...obj_1};
 // console.log(obj_2);
+let queQuan = {
+    "Tỉnh": "Tiền Giang",
+    "Huyện": "Cai Lậy",
+    "Xã": "Long Tiên",
+    "Số nhà": 512
+}
+let nguoiThan = {
+    "Cha": "Mai Văn A",
+    "Mẹ": "Lê Thị B"
+}
+let thanhDanh = {
+    "Tên": "Mai Thành Danh",
+    "Tuổi": 20
+}
+// Muốn Cập nhật object thanhDanh ---> có thêm người thân và quê quán cho ảnh
+thanhDanh = {...thanhDanh, ...queQuan, ...nguoiThan};
+console.log(thanhDanh);
