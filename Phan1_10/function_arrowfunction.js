9c8e6f64d2d748662a6cebcd883bfcc3552bd7c1
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 07:52:07 +0700

    auto-commit

--- Phan1_10/function_arrowfunction.js
+++ Phan1_10/function_arrowfunction.js
@@ -8,3 +8,10 @@ const tinhTong_2 = (n) => {
     return (n*(n+1))/2;
 }
 const tinhTong_3 = n => (n*(n+1))/2;
+// Cho chiều dài = x; rộng = y. tính s
+function tinhDienTich_1(x, y) {
+    return x*y;
+}
+const tinhDienTich_2 = (x, y) =>  x*y;
+console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_1(5, 3)}`);
+console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_2(5, 3)}`);
