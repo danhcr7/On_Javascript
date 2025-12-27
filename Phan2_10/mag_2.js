1a59d382e91399e9cd0df69f7105c969df864464
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 19:44:35 +0700

    auto-commit

--- Phan2_10/mag_2.js
+++ Phan2_10/mag_2.js
@@ -2,3 +2,12 @@ let a1 = [1, 2, 3];
 let a2 = [4, 5, 6];
 let a3 = [...a1, ...a2];
 console.log(a3);
+
+// Copy object:
+let obj_1 = {
+    "name": "David",
+    "old": 30,
+    "salary": 3000000
+};
+let obj_2 = {...obj_1};
+console.log(obj_2);
