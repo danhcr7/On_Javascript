882171b3e7501bc86ef88cacfbb8b6d95e0376a8
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 07:55:31 +0700

    auto-commit

--- Phan1_10/function_arrowfunction.js
+++ Phan1_10/function_arrowfunction.js
@@ -1,20 +1,24 @@
 let n = 10;
 // Cách 1:
 function tinhTong(n) {
-    return (n*(n+1))/2;
+  return (n * (n + 1)) / 2;
 }
 // cách 2:
 const tinhTong_2 = (n) => {
-    return (n*(n+1))/2;
-}
-const tinhTong_3 = n => (n*(n+1))/2;
+  return (n * (n + 1)) / 2;
+};
+const tinhTong_3 = (n) => (n * (n + 1)) / 2;
 // Cho chiều dài = x; rộng = y. tính s
 function tinhDienTich_1(x, y) {
-    return x*y;
+  return x * y;
 }
-const tinhDienTich_2 = (x, y) =>  x*y;
-console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_1(5, 3)}`);
-console.log(`Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_2(5, 3)}`);
+const tinhDienTich_2 = (x, y) => x * y;
+console.log(
+  `Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_1(5, 3)}`
+);
+console.log(
+  `Diện tích hình chữ nhật với dài  = 5, rộng là 3 là: ${tinhDienTich_2(5, 3)}`
+);
 /*
 Bài tập 2: Xử lý chuỗi:
 Yêu cầu: Viết một Arrow Function tên là getHelloName(name).
