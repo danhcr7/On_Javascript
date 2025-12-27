9e33415c105b0480d6a54252f503cecb0f2fe411
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 11:21:49 +0700

    auto-commit

--- Phan2_10/mang_0.js
+++ Phan2_10/mang_0.js
@@ -5,6 +5,7 @@
 # Trong Javascript: string, number, boolean là bất biến
 - Truy cập phần tử của mảng thông qua index: arr[index];
 - Lấy độ dài mảng: arr.length;
+- Phần tử đầu tiên: arr[0]; phần tử cuối cùng: arr[arr.length-1];
 */
 let a = [1, 2, 3];
 let b = [4.2, 5, 6.4]
@@ -12,3 +13,4 @@ let c = [1, 3.2, false, null, {key: 1}, undefined];
 console.log(a, b, c);
 console.log(b[1]);
 console.log(`Độ dài của các mảng là a: ${a.length}; b: ${b.length}, c: ${c.length}`);
+console.log(`Mảng c có phần tử đầu tiên là: ${c[0]}; phần tử cuối cùng là: ${c[c.length-1]}`);
