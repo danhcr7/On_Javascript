f47a1bf0bce5a4734b07034bebafcde9d3bce369
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 11:45:36 +0700

    auto-commit

---
+++ Phan2_10/forEach.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5];
+// Duyệt qua từng phần tử và áp dụng hàm lên pt đó ---> ko biến đổi, không return về giá trị mới 
+/*
+VD1: duyệt qua các phần tử mảng arr và in ra câu đây là lần số ? in ra
+*/
+let i = 0;
+arr.forEach(item => comsole.log(`Đây là lần in thứ ${i++}`));
