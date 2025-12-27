0a5978c0b8f07eac04a836700b4a34aade9c1ed7
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 07:28:13 +0700

    auto-commit

--- Phan1_10/toan_tu_3ngoi.js
+++ Phan1_10/toan_tu_3ngoi.js
@@ -2,8 +2,6 @@ let x = 10;
 // Yêu cầu: Cho biến n = 10. Nếu n chia hết cho 2 thì in ra "Số chẵn", ngược lại "Số lẻ".
 (x%2 === 0) ? console.log("Số chẵn") : console.log("Số lẻ");
 /*
-
-
 📝 Bài tập 2: Kiểm tra đăng nhập (Thực tế Frontend)
 Giả sử Danh làm chức năng ẩn/hiện nút Logout trên giao diện.
 
