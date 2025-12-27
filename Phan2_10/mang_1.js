7ae80738de04e8902b78bb2864c42e24e7416f37
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 11:32:55 +0700

    auto-commit

--- Phan2_10/mang_1.js
+++ Phan2_10/mang_1.js
@@ -3,3 +3,6 @@ let arr = [1, 2, 3, 4, false, null, {a: 1, b: 2, c: 3}, [1, null, undefined]];
 // thêm phần tử "hello" vào cuối mảng:
 arr.push("hello");
 console.log(arr);
+// thêm phần tử [4, {1: "Nam", 2: "Danh"}] vào đầu arr
+arr.unshift([4, {1: "Nam", 2: "Danh"}]);
+console.log(arr);
