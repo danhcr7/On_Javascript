ba4dfd35fd367265d418e21d93bb58bca15b5b09
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 09:17:01 +0700

    auto-commit

--- Phan1_10/function_arrowfunction_1.js
+++ Phan1_10/function_arrowfunction_1.js
@@ -1,7 +1,12 @@
-// Bài 1️⃣ – Tính tổng
-
-// Viết function thường nhận vào 2 số, trả về tổng.
-
-// 📌 Yêu cầu:
-// – Có return
-// – Gọi hàm ra kết quả
+/* Bài 1️⃣ – Tính tổng
+Viết function thường nhận vào 2 số, trả về tổng.
+📌 Yêu cầu:
+ – Có return
+ – Gọi hàm ra kết quả 
+ */
+let a = 4, b = 5;
+function tinhtong_1(a, b) {
+    return (a+b);
+}
+const tinhtong_2 = (a, b) => a + b;
+console.log(`Tổng 2 số ${a} và ${b} là: ${tinhtong_1(a, b)}`);
