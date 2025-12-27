1e77d81c0dc228db98a70556d0aa46bee5d3e194
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 20:01:39 +0700

    auto-commit

---
+++ Phan2_10/baitap/bt1.js
@@ -0,0 +1,11 @@
+/*
+Bài 1️⃣ – Truy cập mảng
+Cho:
+const arr = [10, 20, 30, 40];
+👉 In ra:
+– Phần tử đầu
+– Phần tử cuối
+– Độ dài mảng
+*/
+const arr = [10, 20, 30, 40];
+console.log(`Phần  tử đầu: ${arr[0]}; Phần tử cuối: ${arr[arr.length-1]}; Độ dài mảng: ${arr.length}`);
