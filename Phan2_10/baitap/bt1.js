e3145330c8fbd9346bbf8d9229d0189523436707
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 20:08:59 +0700

    auto-commit

--- Phan2_10/baitap/bt1.js
+++ Phan2_10/baitap/bt1.js
@@ -12,6 +12,9 @@ console.log(`Phần tử đầu: ${arr[0]}; Phần tử cuối: ${arr[arr.length
 // BT2: dùng map tạo mảng mới mà mỗi phần tử nhân 2
 const arr_new = arr.map(item => item * 2);
 console.log(arr_new);
-// BT3: lọc ra các số > 20
+// BT3: Lọc ra các số > 20
 const arr_new_1 = arr.filter(item => (item>20));
 console.log(arr_new_1);
+// BT4: Tìm số đầu tiên > 25:
+const arr_new_2 = arr.find(item => item>25);
+console.log(arr_new_2);
