dcf4bc440d26177512f687a313c14a3d8afdc392
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 11:19:23 +0700

    auto-commit

--- Phan2_10/mang_0.js
+++ Phan2_10/mang_0.js
@@ -4,9 +4,11 @@
 - Chỉ số index: 0 ---> arr.length-1
 # Trong Javascript: string, number, boolean là bất biến
 - Truy cập phần tử của mảng thông qua index: arr[index];
+- Lấy độ dài mảng: arr.length;
 */
 let a = [1, 2, 3];
 let b = [4.2, 5, 6.4]
 let c = [1, 3.2, false, null, {key: 1}, undefined];
 console.log(a, b, c);
 console.log(b[1]);
+console.log(`Độ dài của các mảng là a: ${a.length}; b: ${b.length}, c: ${c.length}`);
