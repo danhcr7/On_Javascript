ee82d9047d89e772d48dec66b7b101a65a3dd4a5
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 11:14:20 +0700

    auto-commit

--- Phan2_10/mang_0.js
+++ Phan2_10/mang_0.js
@@ -1,4 +1,9 @@
 /*
-
-
+- Mảng là 1 tập hợp các phần tử đa kdl, các phần tử có thứ tự
+- VD: let arr = [1, 2.3, true, undefined, null, ["a", "b", "c"], {"Nam": 20, "Tuấn": 30}]
+- Chỉ số index: 0 ---> arr.length-1
 */
+let a = [1, 2, 3];
+let b = [4.2, 5, 6.4]
+let c = [1, 3.2, false, null, {key: 1}, undefined];
+console.log(a, b, c);
