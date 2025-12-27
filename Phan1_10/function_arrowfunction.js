c53326baa1e3abdd50671ea9fd02deb23c5b8422
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-27 07:47:52 +0700

    auto-commit

---
+++ Phan1_10/function_arrowfunction.js
@@ -0,0 +1,10 @@
+let n = 10;
+// Cách 1:
+function tinhTong(n) {
+    return (n*(n+1))/2;
+}
+// cách 2:
+const tinhTong_2 = (n) => {
+    return (n*(n+1))/2;
+}
+const tinhTong_3 = n => (n*(n+1))/2;
