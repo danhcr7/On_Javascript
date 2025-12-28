c4a0a8b2afa7c7676294b3e11ca65ae3705c6db9
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-28 11:31:13 +0700

    auto-commit

--- Phan2_10/baitap/bt3.js
+++ Phan2_10/baitap/bt3.js
@@ -4,3 +4,8 @@ Cho mảng [10, 20, 30, 40] => dùng hàm Filter() để lọc ra các số > 20
 let arr = [10, 20, 30, 40];
 let arr1 = arr.filter(item => (item>20) && item);
 console.log(arr1);
+
+
+// dùng hàm find tìm số đầu tiên > 25
+let kq = arr.find(item => item>25 && item);
+console.log(kq);
