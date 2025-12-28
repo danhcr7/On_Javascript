67b652f5c0ac9bd21a32a21ff1e4010a77a2a6f7
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-28 11:29:15 +0700

    auto-commit

--- Phan2_10/baitap/bt3.js
+++ Phan2_10/baitap/bt3.js
@@ -2,10 +2,5 @@
 Cho mảng [10, 20, 30, 40] => dùng hàm Filter() để lọc ra các số > 20
 */
 let arr = [10, 20, 30, 40];
-function check(item) {
-    if (item > 20) {
-        return item;
-    }
-}
-let arr1 = arr.filter(check(item));
+let arr1 = arr.filter(item => (item>20) && item);
 console.log(arr1);
