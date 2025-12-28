2549bbbaf39e80178f980082c6300b3892b4d84d
Danh <danh_dth235621@student.agu.edu.vn>
2025-12-28 11:26:45 +0700

    auto-commit

---
+++ Phan2_10/baitap/bt3.js
@@ -0,0 +1,10 @@
+/*
+Cho mảng [10, 20, 30, 40] => dùng hàm Filter() để lọc ra các số > 20
+*/
+let arr = [10, 20, 30, 40];
+function check(item) {
+    if (item > 20) {
+        return item;
+    }
+}
+let arr1 = arr.filter(check(item));
